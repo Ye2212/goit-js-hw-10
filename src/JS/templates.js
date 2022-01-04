382d87db75f819fe8fe0ba67dac4ed0bcdf03ad9
Yevheniia Ukolova <ukolova_e@ukr.net>
2022-01-04 13:48:04 +0200

    udate tampletes.js, styles.css

--- src/JS/templates.js
+++ src/JS/templates.js
@@ -7,16 +7,24 @@ export function showCountryList ({ flags, name }){
     `
 }
 
-export function showCountryCard ({ flags, name, capital, population, languiges}){
+export function showCountryCard ({ flags, name, capital, population, languages}){
     return `
-    <img class='country-flag' src='${flags.svg}' alt='${name.official}' width=100 />
-    <h2 class='country-title'>${name.official}</h2>
+    <div class='wrapper'>
+        <img class='country-flag' src='${flags.svg}' alt='${name.official}' width=100 />
+        <h2 class='country-title'>${name.official}</h2>
+     </div>
+    <div class='wrapper'>
         <p class='country-text'>Capital:</p>
         <span class='country-subtext'>${capital}</span>
+    </div>
+    <div class='wrapper'>
         <p class='country-text'>Population:</p>
         <span class='country-subtext'>${population}</span>
-        <p class='country-text'>Languiges:</p>
-        <span class='country-subtext'>${languiges}</span>
+    </div>
+    <div class='wrapper'>
+        <p class='country-text'>Languages:</p>
+        <span class='country-subtext'>${Object.values(languages)}</span>
+    </div>
     `
 
 }
